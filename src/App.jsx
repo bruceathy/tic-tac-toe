ae22a3339b7221e5fb0a2c9ba02873382014e909
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-11-13 07:46:29 -0500

    Ball & Sword svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Player from "./components/Player";
+import Swords from "./components/Swords";
+import Ball from "./components/Ball";
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          <Player name="Player 1" symbol={<Swords />} />
+          <Player name="Player 2" symbol={<Ball />} />
         </ol>
         {/* GAME BOARD */}
       </div>
