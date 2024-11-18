50cbb669a683b7a07f9aaa3f410862eb7f1a0259
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-11-17 19:50:43 -0500

    update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <li>
       <span className="player">
         <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={() => setIsEditing(true)}>Edit</button>
     </li>
   );
 }
