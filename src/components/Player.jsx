e9e304df34c867017d0ad6b871bf789c1a82b4f8
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-11-20 22:17:41 -0500

    up

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick(e) {
-    setIsEditing(()=>);
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
