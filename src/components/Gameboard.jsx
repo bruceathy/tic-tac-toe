6f1a3abe304e5ebf7a057c62c04c61abbc5f67af
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-12-02 10:48:14 -0500

    gameboard reactivity

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -11,19 +11,24 @@ export default function Gameboard() {
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameboard((prevGameBoard) => {
-      prevGameBoard[rowIndex][colIndex] = "X";
-      return prevGameBoard;
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
     });
   }
 
   return (
     <ol id="game-board">
-      {initialGameboard.map((row, rowIndex) => (
+      {gameboard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
